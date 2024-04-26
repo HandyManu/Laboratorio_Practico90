425a495db9daf0e873a418daf4fbe68a4949a444
Manuel <20220416@ricaldone.edu.sv>
2024-04-26 13:48:17 -0600

    El principio

---
+++ app/src/main/java/Manuel/Ortega/emulador_20220416_manuel/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package Manuel.Ortega.emulador_20220416_manuel.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
